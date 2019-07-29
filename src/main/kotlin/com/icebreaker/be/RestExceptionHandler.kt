629b7544679eac39c49d89089872612e46fff7e6
alexey <khlebtsov@gmail.com>
2019-07-29 19:30:23 +0200

    implements controller advice

--- src/main/kotlin/com/icebreaker/be/RestExceptionHandler.kt
+++ src/main/kotlin/com/icebreaker/be/RestExceptionHandler.kt
@@ -3,10 +3,12 @@ package com.icebreaker.be
 import com.icebreaker.be.exception.CreditsNotAvailableException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 
+@ControllerAdvice
 class RestExceptionHandler : ResponseEntityExceptionHandler() {
     @ExceptionHandler(CreditsNotAvailableException::class)
     fun handleCreditsNotAvailableException(
