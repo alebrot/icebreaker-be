96b5197102981b94e0a1167f41aaaf16ce0dc0c7
alexey <khlebtsov@gmail.com>
2019-12-07 20:16:32 +0100

    sets up in memory broker

--- src/main/kotlin/com/icebreaker/be/config/WebSocketConfig.kt
+++ src/main/kotlin/com/icebreaker/be/config/WebSocketConfig.kt
@@ -1,33 +1,12 @@
 package com.icebreaker.be.config
 
-import com.icebreaker.be.WebSocketProperties
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
-@Profile("prod")
-@EnableWebSocketMessageBroker
-class WebSocketConfig(val webSocketProperties: WebSocketProperties) : WebSocketMessageBrokerConfigurer {
-    override fun configureMessageBroker(config: MessageBrokerRegistry) {
-        config.enableStompBrokerRelay("/chat")
-                .setRelayHost(webSocketProperties.relayHost)
-                .setRelayPort(webSocketProperties.relayPort)
-                .setClientLogin(webSocketProperties.clientLogin)
-                .setClientPasscode(webSocketProperties.clientPasscode);
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/chat-websocket").setAllowedOrigins("*").withSockJS()
-    }
-}
-
-@Configuration
-@Profile("!prod")
 @EnableWebSocketMessageBroker
 class WebSocketConfigInMemory : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
