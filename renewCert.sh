66e47d77ab33a6fdb1942320c003e413ee890694
alexey <khlebtsov@gmail.com>
2019-12-08 19:08:11 +0100

    updates renewCert.sh

--- renewCert.sh
+++ renewCert.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-CERT_BOT=/usr/local/bin/certbot-auto
 LETS_ENCRYPT_PATH=/etc/letsencrypt/live/kofify.com
-TO_CERT_JAVA_FILE="/var/springboot/app/mycert.p12"
+TO_CERT_JAVA_FILE=/var/springboot/app/mycert.p12
 FROM_CERT_JAVA_FILE="${LETS_ENCRYPT_PATH}/mycert.p12"
 
-CERT_BOT renew --dry-run
+/usr/local/bin/certbot-auto renew --dry-run
 
 echo "generating java certificate from pem"
 openssl pkcs12 -export -in "${LETS_ENCRYPT_PATH}/fullchain.pem" -inkey "${LETS_ENCRYPT_PATH}/privkey.pem" -out FROM_CERT_JAVA_FILE -name "mycert" -CAfile "${LETS_ENCRYPT_PATH}/chain.pem" -caname root -password pass:password
